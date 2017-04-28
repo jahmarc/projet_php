eb1ac77b5d734c5e8cf4dcd165b40bed24eebd96
Helena <helena.pereira@students.hevs.ch>
2017-04-28 14:56:44 +0200

    dd

--- projet_php/views/login/welcome.php
+++ projet_php/views/login/welcome.php
@@ -12,58 +12,59 @@ $user = $_SESSION['user'];
 
 
 <br><br>
-	<div align="center" style="margin-top:20px;"><?php echo $msg;?>
-	
-	<div>
+	<div style="display: inline-block; margin-top:20px;">
 		<!-- <a href="<?php echo URL_DIR.'game/game_test';?>">New Game</a>7-->
 		<input type="button" class="OK" value="NEW GAME"/>
 		<input type="button" class="OK" value="JOIN A PARTY"/>	
 	</div>
 	
-	
-	
-	<h1>Welcome <?php echo '<a style="color:#00a1ff;"> '.$user->getFirstname().' '.$user->getLastname();?></a>	</h1>	
-	<h2>The list of registered users</h2>
-	<?php
-	
-	$servername = "localhost";
-	$username = "root";
-	$password = "";
-	$dbname = "php_mvc";
-	
-	// Create connection
-	$conn = new mysqli($servername, $username, $password, $dbname);
-	
-	// Check connection
-	if ($conn->connect_error) {
-		die("Connection failed: " . $conn->connect_error);
-	}
-	
-	$sql = "SELECT id, firstname, lastname, username FROM user";
-	$result = $conn->query($sql);
-	
-	?>
-	
-	<table id="tableLogin" style="margin-top:50px; width:40%;">
-	
-	<?php
-	while($row = $result->fetch_assoc()) 
-	{
-		echo "	<tr>
-					<td>USER : " . $row["username"]. "</td>
-			  	</tr>
-				<tr>
-					<td style='padding-left:10%;'> NAME : " . $row["firstname"]. "</td>
-					<td	> SURNAME : " . $row["lastname"]. "</td>
-				</tr>
-			
-			<tr style='margin-top:50px;'><td></td></tr>";
-	}
-	
-	 
-	?>
-	</table>
-	
+	<div align="center" style="margin-top:20px;"><?php echo $msg;?>
+
+		
+		<h1>Welcome <?php echo '<a style="color:#00a1ff;"> '.$user->getFirstname().' '.$user->getLastname();?></a>	</h1>	
+		<h2>The list of registered users</h2>
+		<?php
+		
+		$servername = "localhost";
+		$username = "root";
+		$password = "";
+		$dbname = "php_mvc";
+		
+		// Create connection
+		$conn = new mysqli($servername, $username, $password, $dbname);
+		
+		// Check connection
+		if ($conn->connect_error) {
+			die("Connection failed: " . $conn->connect_error);
+		}
+		
+		$sql = "SELECT id, firstname, lastname, username FROM user";
+		$result = $conn->query($sql);
+		
+		?>
+		
+		<table id="tableLogin" style="margin-top:50px; width:40%;">
+		
+		<?php
+		while($row = $result->fetch_assoc()) 
+		{
+			echo "
+						<td>USER : " . $row["username"]. "</td><tr>
+				  	</tr>
+					<tr>
+						<td style='padding-left:10%;'> NAME : " . $row["firstname"]. "</td>
+						<td	> SURNAME : " . $row["lastname"]. "</td>
+					</tr>
+				<tr><td><hr></td><td><hr></td></tr>
+				
+				
+				<tr style='margin-top:50px;'><td></td></tr>";
+		}
+		
+		 
+		?>
+		</table>
+		
 	
 	</div>	
 	

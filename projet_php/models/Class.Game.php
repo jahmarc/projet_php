ee0fa8da9a0d2c9611d7a2de9b9bc04ade3b6aac
Stéphanie Pinto <stephanie.pinto1@students.hevs.ch>
2017-05-08 17:30:49 +0200

    Game

---
+++ projet_php/models/Class.Game.php
@@ -0,0 +1,84 @@
+<?php
+class Game{
+
+	
+	
+	
+	
+	public function __construct(){
+		
+	}
+	
+	/**
+	 * Renvoi la donne en cours
+	 */
+	public function  GetCurrentDonne(){
+		
+	}
+	
+	
+	/**
+	 * Renvoi le pli en cours
+	 */
+	public function  GetCurrentPli(){
+	
+	}
+	
+	
+	/**
+	 * Renvoi le joueur qui doit jouer
+	 */
+	public function  GetCurrentPlayer(){
+	
+	}
+	
+	
+	/**
+	 * Renvoi l'atoût en cours
+	 */
+	public function  GetCurrentAsset(){
+	
+	}
+	
+	
+	/**
+	 * Renvoi les cartes qu'il vous reste à jouer
+	 */
+	public function  GetMyCards(){
+	
+	}
+	
+	
+	/**
+	 * Renvoi les points de la partie en cours
+	 */
+	public function  GetPointsPart(){
+	
+	}
+	
+	
+	/**
+	 * Renvoi les points de la donne en cours
+	 */
+	public function  GetPointsDonne(){
+	
+	}
+	
+	
+	/**
+	 * Renvoi les conversations de la partie en cours
+	 */
+	public function  GetChat(){
+	
+	}
+	
+	
+	/**
+	 * Met à jour toutes les données nécessaires à la partie
+	 * 
+	 */
+	public function  RefreshPart($idPart, $idUser, $ModifyOnAt){
+	
+	}
+	
+}
